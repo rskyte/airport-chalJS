fef76813c788be3fe425f9aeb2e5789c16ad1b69
Rhys Kyte <rskyte@gmail.com>
2017-12-19 16:06:00 +0000

    planes can take off

--- jasmine-standalone-2.8.0/spec/airportSpec.js
+++ jasmine-standalone-2.8.0/spec/airportSpec.js
@@ -2,13 +2,28 @@
 describe("Airport", function() {
   var airport;
   var plane;
+  var plane2;
 
   beforeEach(function () {
     airport = new Airport();
     plane = new Plane();
+    plane2 = new Plane();
   });
 
 it("should allow planes to land", function() {
   expect(airport.land(plane)).toEqual(plane)
 })
+
+it("should save the plane in a list of planes", function() {
+  airport.land(plane)
+  expect(airport.planes).toEqual([plane])
+})
+
+it("allows specific planes to takeoff", function() {
+  airport.land(plane)
+  airport.land(plane2)
+  airport.takeoff(plane)
+  expect(airport.planes).toEqual([plane2])
+})
+
 })
--- jasmine-standalone-2.8.0/src/airport.js
+++ jasmine-standalone-2.8.0/src/airport.js
@@ -1,5 +1,13 @@
-function Airport () {}
+function Airport () {
+  this.planes = []
+
+}
 
 Airport.prototype.land = function(plane) {
+  this.planes.push(plane)
   return plane
+}
+
+Airport.prototype.takeoff = function(plane) {
+  this.planes.pop(plane)
 }
